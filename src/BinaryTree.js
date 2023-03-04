323a8bb6632ed8e3b39939e0c89825383509366a
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2023-03-04 13:46:12 -0800

    burd

--- src/BinaryTree.js
+++ src/BinaryTree.js
@@ -7,22 +7,34 @@ const BinaryTree = () => {
     name: 'Root',
     children: []
   });
+  const [treeKey, setTreeKey] = useState(0);
 
   const handleClick = () => {
-    let index = 0;
+    const maxDepth = 3;
+    let subtreeCount = 0;
     const addSubtree = (node, depth) => {
-      index++;
-      if (depth < 3) {
-        node.children.push({
-          name: `Subtree ${index}`,
-          children: []
-        });
-        addSubtree(node.children[node.children.length - 1], depth + 1);
-        addSubtree(node.children[node.children.length - 1], depth + 1);
+      if (depth === maxDepth) {
+        return;
       }
+      subtreeCount++;
+      const leftChild = {
+        name: `Subtree ${subtreeCount}-left`,
+        key: `Subtree ${subtreeCount}-left-${depth}`,
+        children: []
+      };
+      const rightChild = {
+        name: `Subtree ${subtreeCount}-right`,
+        key: `Subtree ${subtreeCount}-right-${depth}`,
+        children: []
+      };
+      node.children.push(leftChild);
+      node.children.push(rightChild);
+      addSubtree(leftChild, depth + 1);
+      addSubtree(rightChild, depth + 1);
     };
     addSubtree(treeData, 0);
     setTreeData({...treeData});
+    setTreeKey(treeKey + 1);
   };
 
   return (
@@ -30,18 +42,15 @@ const BinaryTree = () => {
         <button onClick={handleClick}>Grow tree</button>
 
         <Tree
-            data={{
-              children: [
-                {},
-                {}
-              ],
-            }}
+            key={treeKey}
+            data={treeData}
             height={400}
             svgProps={{
               transform: 'rotate(90)'
             }}
             width={400}
         />
+
       </div>
   );
 };
