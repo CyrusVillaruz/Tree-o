f10feb0a58ee0266c6505a48735ba69019937cd6
CyrusVillaruz <cyrusvillaruz1@gmail.com>
2023-03-04 14:25:20 -0800

    functional root growth

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,24 +1,30 @@
 import React, { useRef, useState } from "react";
 
-function TextBox({ onInsert }) {
-  const [value, setValue] = useState(0);
-  const handleChange = (e) => setValue(+e.target.value);
+function TextBox({ onInsert }) {  
+  var temp = 5000;
+  const [value, setValue] = useState(temp);
+  // const handleChange = (e) => setValue(+e.target.value);
+  let button = document.querySelector('button');
+
+
   const handleInsert = () => {
-    setValue(0);
+    setValue((Math.ceil(Math.random(0, 10000) * 10000)));
     onInsert(value);
     inputRef.current.focus();
+    button.innerText = "Grow the tree!";
   };
+
   const inputRef = useRef(null);
 
   return (
     <div>
-      <input
+      {/* <input
         ref={inputRef}
         onChange={handleChange}
         type="number"
         value={value}
-      />
-      <button onClick={handleInsert}>Insert</button>
+      /> */}
+      <button id='seedButton'onClick={handleInsert}>Plant the seed!</button>
     </div>
   );
 }
