a4ae7bc33803e723f5060b79da74636b59b07344
Nicolas Rodriguez <nrodriguez8@my.bcit.ca>
2023-03-04 15:28:49 -0800

    allow for button to update after first click

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -1,31 +1,22 @@
 import React, { useRef, useState } from "react";
 
-function TextBox({ onInsert }) {  
+function TextBox({ onInsert }) {
   var temp = 5000;
   const [value, setValue] = useState(temp);
-  // const handleChange = (e) => setValue(+e.target.value);
-  let button = document.querySelector('button');
-
 
   const handleInsert = () => {
     setValue((Math.ceil(Math.random(0, 10000) * 10000)));
     onInsert(value);
-    inputRef.current.focus();
+    const button = document.querySelector('#seedButton');
     button.innerText = "Grow the tree!";
   };
 
   const inputRef = useRef(null);
 
   return (
-    <div>
-      {/* <input
-        ref={inputRef}
-        onChange={handleChange}
-        type="number"
-        value={value}
-      /> */}
-      <button id='seedButton'onClick={handleInsert}>Plant the seed!</button>
-    </div>
+      <div>
+        <button id='seedButton'onClick={handleInsert}>Plant the seed!</button>
+      </div>
   );
 }
 
